7b3cce7566cb58a62ebcf2a4111d6decb44b1e9f
Brandon Steigerwald <bmsteiger@gmail.com>
2021-02-11 11:00:38 -0600

    nvidia partner search started

--- api_get_test.py
+++ api_get_test.py
@@ -5,6 +5,7 @@ import requests
 from dotenv import load_dotenv
 from os import path, getenv, system
 import random
+from pprint import pprint
 
 load_dotenv()
 MIN_DELAY = int(getenv('MIN_DELAY'))
@@ -17,16 +18,48 @@ def main():
     "Main Function Block"
     url = ""
     api_url1 = "https://api.nvidia.partners/edge/product/search?page=1&limit=900&locale=en-us&sorting=lp&category=GPU&category_filter=GPU~77,Laptop~139,Studio-Laptop~13,NVLINKS~1"
-    resp = nvidia_partners_get(url, api_url1)
+    retailer = nvidia_partners_get(url, api_url1)
+    for retailer in retailers:
+        search_nvidia_partner(retailer)
 
 
-def sleep_random_length():
-    base_sleep = 1
-    total_sleep = base_sleep + random.uniform(MIN_DELAY, MAX_DELAY)
-    sleep(1)
+def search_nvidia_partner(retailer):
+    "Check on the retailer's website for stock"
+    retailer_name = retailer.get("retailerName")
+    if 'https://www.microcenter.com' in retailer_name:
+        print(
+            f"{retailer_name} check from NVIDIA Parner API result is not implemented yet"
+        )
+    if 'https://store.asus.com' in retailer_name:
+        print(
+            f"{retailer_name} check from NVIDIA Parner API result is not implemented yet"
+        )
+    if 'https://www.bhphotovideo.com' in retailer_name:
+        print(
+            f"{retailer_name} check from NVIDIA Parner API result is not implemented yet"
+        )
+    if 'https://www.newegg.com' in retailer_name:
+        print(
+            f"{retailer_name} check from NVIDIA Parner API result is not implemented yet"
+        )
+    if 'https://bestbuy.com' in retailer_name:
+        print(
+            f"{retailer_name} check from NVIDIA Parner API result is not implemented yet"
+        )
+
+        pass
+    pass
 
 
-def nvidia_partners_get(_, api_url) -> list:
+def sleep_random_length(base_sleep=1,
+                        min_delay=MIN_DELAY,
+                        max_delay=MAX_DELAY):
+    "Sleep a random length of time"
+    total_sleep = base_sleep + random.uniform(min_delay, max_delay)
+    sleep(total_sleep)
+
+
+def nvidia_partners_get(display_names, api_url) -> list:
     """Returns a list of product dicts from the NVIDIA Partners API
     Expected fields this function uses:  
         productTitle: Title of the entry on the NVIDIA page
@@ -52,13 +85,22 @@ def nvidia_partners_get(_, api_url) -> list:
     print(f"{len(products)} Products from API:")
     for product in products:
         product_title = product.get("productTitle")
+
         print(product_title)
+        for retailer in product.get("retailers"):
+            print(f'\tRetailer Name: {retailer.get("retailerName")}')
+            # print(f'\tPurchase Link: {retailer.get("purchaseLink")}')
+            # print(
+            #     f'\tDirect Purchase Link: {retailer.get("directPurchaseLink")}'
+            # )
+
         if product_title.lower() in [name.lower() for name in display_names]:
             retailers = product.get("retailers")
             for retailer in retailers:
                 nvidia_api_result.append(retailer)
 
-    return nvidia_api_result
+    print(f"{len(nvidia_api_result)} matching products found")
+    return nvidia_api_result, products
 
 
 if __name__ == "__main__":
