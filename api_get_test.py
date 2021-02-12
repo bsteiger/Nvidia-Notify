5dcbc22b89774e9414a93a34c439f5faa44073c8
Brandon Steigerwald <bmsteiger@gmail.com>
2021-02-11 18:51:11 -0600

    code cleanup. Still not done: search_nvidia_partner callbacks

--- api_get_test.py
+++ api_get_test.py
@@ -10,7 +10,10 @@ from pprint import pprint
 load_dotenv()
 MIN_DELAY = int(getenv('MIN_DELAY'))
 MAX_DELAY = int(getenv('MAX_DELAY'))
-
+display_names = [
+    "EVGA RTX 3060 Tix", "EVGA RTX 3060 Tip", "NVIDIA GEFORCE RTX 3060 TI",
+    "PNY GeForce RTX 3070 8GB UPRISING Dual Fan"
+]
 get_times = []
 
 
@@ -18,7 +21,7 @@ def main():
     "Main Function Block"
     url = ""
     api_url1 = "https://api.nvidia.partners/edge/product/search?page=1&limit=900&locale=en-us&sorting=lp&category=GPU&category_filter=GPU~77,Laptop~139,Studio-Laptop~13,NVLINKS~1"
-    retailer = nvidia_partners_get(url, api_url1)
+    retailers = nvidia_partners_get(display_names, api_url1)
     for retailer in retailers:
         search_nvidia_partner(retailer)
 
@@ -26,25 +29,28 @@ def main():
 def search_nvidia_partner(retailer):
     "Check on the retailer's website for stock"
     retailer_name = retailer.get("retailerName")
+    purchase_link = retailer.get("purchaseLink")
+    product_title = retailer.get("productTitle")
+    print(f"Checking stock for {product_title} at {retailer_name}...")
     if 'https://www.microcenter.com' in retailer_name:
         print(
-            f"{retailer_name} check from NVIDIA Parner API result is not implemented yet"
+            f"{retailer_name} check from NVIDIA Partner API result is not implemented yet"
         )
     if 'https://store.asus.com' in retailer_name:
         print(
-            f"{retailer_name} check from NVIDIA Parner API result is not implemented yet"
+            f"{retailer_name} check from NVIDIA Partner API result is not implemented yet"
         )
     if 'https://www.bhphotovideo.com' in retailer_name:
         print(
-            f"{retailer_name} check from NVIDIA Parner API result is not implemented yet"
+            f"{retailer_name} check from NVIDIA Partner API result is not implemented yet"
         )
     if 'https://www.newegg.com' in retailer_name:
         print(
-            f"{retailer_name} check from NVIDIA Parner API result is not implemented yet"
+            f"{retailer_name} check from NVIDIA Partner API result is not implemented yet"
         )
     if 'https://bestbuy.com' in retailer_name:
         print(
-            f"{retailer_name} check from NVIDIA Parner API result is not implemented yet"
+            f"{retailer_name} check from NVIDIA Partner API result is not implemented yet"
         )
 
         pass
@@ -60,7 +66,7 @@ def sleep_random_length(base_sleep=1,
 
 
 def nvidia_partners_get(display_names, api_url) -> list:
-    """Returns a list of product dicts from the NVIDIA Partners API
+    """Returns a list of retailer product dicts from the NVIDIA Partners API
     Expected fields this function uses:  
         productTitle: Title of the entry on the NVIDIA page
         purchaseLink: gethatch url that redirects to the purchase url from the retailer
@@ -75,10 +81,6 @@ def nvidia_partners_get(display_names, api_url) -> list:
     # Find the all the product entries, regular and 'featured
     products = res['searchedProducts']['productDetails']
     products.append(res['searchedProducts']['featuredProduct'])
-
-    display_names = [
-        "EVGA RTX 3060 Tix", "EVGA RTX 3060 Tip", "NVIDIA GEFORCE RTX 3060 TI"
-    ]
 
     # find the product we want
     nvidia_api_result = []
@@ -95,12 +97,10 @@ def nvidia_partners_get(display_names, api_url) -> list:
             # )
 
         if product_title.lower() in [name.lower() for name in display_names]:
-            retailers = product.get("retailers")
-            for retailer in retailers:
-                nvidia_api_result.append(retailer)
+            nvidia_api_result.extend(product.get("retailers"))
 
     print(f"{len(nvidia_api_result)} matching products found")
-    return nvidia_api_result, products
+    return nvidia_api_result
 
 
 if __name__ == "__main__":
